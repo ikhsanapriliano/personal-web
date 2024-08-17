a3f6c89880b6734536e8c96b152fd993175d9aba
ikhsan <ikhsanapriliano4@gmail.com>
2024-08-17 19:19:06 +0700

    fix wa number

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -4,7 +4,7 @@ const Contact = () => {
             <h2 className="font-semibold text-xl text-white">Contact Me</h2>
             <div className="flex text-[30px] gap-7">
                 <a href="https://www.linkedin.com/in/ikhsan-apriliano/" target="_blank"><i className="fa-brands text-white fa-linkedin"></i></a>
-                <a href="https://wa.me/081572832083" target="_blank"><i className="fa-brands text-white fa-whatsapp"></i></a>
+                <a href="https://wa.me/+6281572832083" target="_blank"><i className="fa-brands text-white fa-whatsapp"></i></a>
                 <a href="mailto:ikhsanapriliano4@gmail.com"><i className="fa-solid text-white fa-envelope"></i></a>
             </div>
         </section>
